e79a6793152d2cfc181e2f5b3c18b31d69359df4
RexYing <rex.ying0923@gmail.com>
2018-03-13 18:23:15 -0700

    train

--- graphsage.py
+++ graphsage.py
@@ -5,21 +5,23 @@ from torch.autograd import Variable
 import numpy as np
 
 class SupervisedGraphSage(nn.Module):
+    ''' GraphSage embeddings
+    '''
 
     def __init__(self, num_classes, enc):
         super(SupervisedGraphSage, self).__init__()
         self.enc = enc
         self.xent = nn.CrossEntropyLoss()
 
-        self.weight = nn.Parameter(torch.FloatTensor(num_classes, enc.embed_dim))
+        self.weight = nn.Parameter(torch.FloatTensor(enc.embed_dim, num_classes))
         init.xavier_uniform(self.weight)
 
     def forward(self, nodes):
         embeds = self.enc(nodes)
-        scores = self.weight.mm(embeds)
-        return scores.t()
+        scores = embeds.mm(self.weight)
+        return scores
 
     def loss(self, nodes, labels):
         scores = self.forward(nodes)
-        return self.xent(scores, labels.squeeze())
+        return self.xent(nn.softmax(scores), labels.squeeze())
 
--- train.py
+++ train.py
@@ -11,8 +11,53 @@ import util
 
 #class ClassifyGraphs():
 
-def syntheticTask1(input_feat_dim=10):
+def synthetic_task_train(graphs, same_feat=True):
+    feat_data, labels, adj_lists = load_cora()
+    if same_feat:
+        feat_data = graphs[0].node[0]['feat']
+        feat_dim = feat_data.shape[0]
+        features = nn.Embedding(feat_dim)
+        features.weight = nn.Parameter(torch.FloatTensor(feat_data), requires_grad=True)
+        # features.cuda()
 
+    agg1 = MeanAggregator(features, cuda=True)
+    enc1 = Encoder(features, 1433, 128, adj_lists, agg1, gcn=True, cuda=False)
+    agg2 = MeanAggregator(lambda nodes : enc1(nodes).t(), cuda=False)
+    enc2 = Encoder(lambda nodes : enc1(nodes).t(), enc1.embed_dim, 128, adj_lists, agg2,
+            base_model=enc1, gcn=True, cuda=False)
+    enc1.num_samples = 5
+    enc2.num_samples = 5
+
+    graphsage = SupervisedGraphSage(7, enc2)
+#    graphsage.cuda()
+    rand_indices = np.random.permutation(num_nodes)
+    test = rand_indices[:1000]
+    val = rand_indices[1000:1500]
+    train = list(rand_indices[1500:])
+
+    optimizer = torch.optim.SGD(filter(lambda p : p.requires_grad, graphsage.parameters()), lr=0.7)
+    times = []
+    for batch in range(100):
+        batch_nodes = train[:256]
+        random.shuffle(train)
+        start_time = time.time()
+        optimizer.zero_grad()
+        loss = graphsage.loss(batch_nodes, 
+                Variable(torch.LongTensor(labels[np.array(batch_nodes)])))
+        loss.backward()
+        optimizer.step()
+        end_time = time.time()
+        times.append(end_time-start_time)
+        print batch, loss.data[0]
+
+    val_output = graphsage.forward(val) 
+    print "Validation F1:", f1_score(labels[val], val_output.data.numpy().argmax(axis=1), average="micro")
+    print "Average batch time:", np.mean(times)
+
+
+def synthetic_task1(input_feat_dim=10):
+
+    # data
     graphs1 = datagen.gen_ba(range(40, 60), range(4, 5), 20, 
                              featgen.ConstFeatureGen(np.ones(input_feat_dim)))
     for G in graphs1:
@@ -25,8 +70,12 @@ def syntheticTask1(input_feat_dim=10):
         G.graph['label'] = 1
     util.draw_graph_list(graphs2[:16], 4, 4, 'figs/ba2')
 
+    graphs = graphs1 + graphs2
+    synthetic_task_train(graphs)
+    
+
 def main():
-    syntheticTask1()
+    synthetic_task1()
 
 if __name__ == "__main__":
     main()
