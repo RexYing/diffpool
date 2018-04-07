383205243830351c11832d3f14c344868e14e084
RexYing <rex.ying0923@gmail.com>
2018-04-06 18:42:30 -0700

    test

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import gen.data as datagen
 from graph_sampler import GraphSampler
 import util
 
+def synthetic_task_test(dataset, args):
 
 def synthetic_task_train(dataset, args, same_feat=True):
 
