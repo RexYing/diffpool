accb1c3a9c6d56f68e2ca875469c2ff4bc1048c6
RexYing <rex.ying0923@gmail.com>
2019-02-07 17:01:30 -0800

    fix num classes

--- example.sh
+++ example.sh
@@ -2,5 +2,5 @@
 python -m train --datadir=data --bmname=DD --cuda=0 --max-nodes=500 --epochs=1000
 
 # ENZYMES
-python -m train --datadir=data --bmname=ENZYMES --cuda=3 --max-nodes=100 --label-classes=6
+python -m train --datadir=data --bmname=ENZYMES --cuda=3 --max-nodes=100 --num-classes=6
 
