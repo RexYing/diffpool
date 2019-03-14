de26e243e96e34567a49f461df255f3767d11b36
Rex Ying <rex.ying0923@gmail.com>
2019-03-14 16:56:20 -0700

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,11 @@
-# DD
-python -m train --datadir=data --bmname=DD --cuda=0 --max-nodes=500 --epochs=1000
-
 # ENZYMES
 python -m train --datadir=data --bmname=ENZYMES --cuda=3 --max-nodes=100 --num-classes=6
 
+# ENZYMES - Diffpool
+python -m train --bmname=ENZYMES --assign-ratio=0.1 --hidden-dim=30 --output-dim=30 --cuda=1 --num-classes=6 --method=soft-assign
+
+# DD
+python -m train --datadir=data --bmname=DD --cuda=0 --max-nodes=500 --epochs=1000 --num-classes=2
+
+# DD - Diffpool
+python -m train --bmname=ENZYMES --assign-ratio=0.1 --hidden-dim=64 --output-dim=64 --cuda=1 --num-classes=2 --method=soft-assign
